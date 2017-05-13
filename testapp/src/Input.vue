5ff5b35b49ca1265668c971029b769b1d3ae20a5
Moe Pad <moepad@mamem.de>
2017-05-13 13:47:50 +0200

    added new component input

---
+++ testapp/src/Input.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <input type='text' v-model='inputText'>
+    <p>{{inputText}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        inputText: ''
+      }
+    }
+  }
+</script>
