5920ad061f248a8ada1df334a5286486fc5efec5
Moe Pad <moepad@mamem.de>
2017-05-13 13:50:42 +0200

    moved component Input to be part of component Message

--- testapp/src/App.vue
+++ testapp/src/App.vue
@@ -1,18 +1,6 @@
 <template>
   <div id="app">
     <app-message></app-message>
-    <app-input></app-input>
   </div>
 </template>
 
-<script>
-  import Input from './Input.vue'
-  export default {
-    components: {
-      'app-input': Input
-    }
-  }
-</script>
-
-<style>
-</style>
--- testapp/src/Message.vue
+++ testapp/src/Message.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
     <h1>This is a message</h1>
+    <app-input></app-input>
   </div>
 </template>
+
+<script>
+  import Input from './Input.vue'
+  export default {
+    components: {
+      'app-input': Input
+    }
+  }
+</script>
+
+<style>
+</style>
