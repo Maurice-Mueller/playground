3996a09d89424be142bde16dc6e44d434d041865
Moe Pad <moepad380@googlemail.com>
2017-06-26 13:30:13 +0200

    added an interface

--- greeter.ts
+++ greeter.ts
@@ -1,7 +1,20 @@
 function greeter(person: string) {
-    return "Hello, " + person;
+    return "Hello, " + person
 }
 
-var user = "Jane User";
+var user = "Jane User"
 
-document.body.innerHTML = greeter(user);
+document.body.innerHTML = greeter(user)
+
+interface Person {
+    firstName: string
+    lastName: string
+}
+
+function greetInterface(person: Person) {
+    return "Hello " + person.firstName + " " + person.lastName;
+}
+
+var interfaceUser = {firstName: "jane", lastName: "user"}
+
+document.body.innerHTML = greetInterface(interfaceUser)
